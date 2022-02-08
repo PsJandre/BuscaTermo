1a85abdec2f0e00ebf38e35f8d1362732d06ecae
PsJandre <pedrootaviosoriano@gmail.com>
2022-02-08 11:59:05 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string frase = "O Rato roeu a roupa do rei de roma";
+            //string termo = "Rato";
+            Console.WriteLine("Digite a frase: ");
+            string frase = Console.ReadLine();
+            Console.WriteLine("Digite o termo a ser buscado: ");
+            string palavraBuscada = Console.ReadLine();
+            frase = frase.ToLower();
+            palavraBuscada = palavraBuscada.ToLower();
+            string[] palavras = frase.Split(" ");
+            int count = 0;
+            foreach (var palavra in palavras){
+                if (palavra.Equals(palavraBuscada))
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("O termo foi encontrado "+count+" vezes na frase");
+        }
+    }
+}
